df60f1874c168dc0932d551f8c6629c521f34ff0
zoahl23 <hlan22102003@gmail.com>
2024-03-06 07:41:34 +0700

    feature: tim min va tim max

--- function.cpp
+++ function.cpp
@@ -3,3 +3,7 @@ int find_min(int *arr, int length);
 {
 // code
 }
+int find_max(int *arr, int length)
+{
+// code
+}
--- function.h
+++ function.h
@@ -1,3 +1,4 @@
 #include <iostream>
 using namespace std;
 int find_min(int *arr, int length);
+int find_max(int *arr, int length);
