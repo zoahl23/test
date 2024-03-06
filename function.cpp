34fd91917a6686fa8ed2e8066163b6f6c3684612
zoahl23 <hlan22102003@gmail.com>
2024-03-06 07:43:29 +0700

    feature: tim min, tim max va sap xep

--- function.cpp
+++ function.cpp
@@ -7,3 +7,7 @@ int find_max(int *arr, int length)
 {
 // code
 }
+int SORT(int *arr, int length)
+{
+// SORT cua nhanh feature
+}
--- function.h
+++ function.h
@@ -2,3 +2,4 @@
 using namespace std;
 int find_min(int *arr, int length);
 int find_max(int *arr, int length);
+int SORT(int *arr, int length);
